bae90487506c4415b1db5edd2c11dc76fd852145
murawakimitsuhiro <mitsuhiromurawaki@gmail.com>
2019-04-18 18:16:09 +0900

    Update ping 以外を動作させなくした

--- main.go
+++ main.go
@@ -3,33 +3,36 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"log"
 	"net/http"
-	"os"
 
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
-	"github.com/kelseyhightower/envconfig"
-
 	"github.com/murawakimitsuhiro/go-simple-RESTful-api/driver"
 	"github.com/murawakimitsuhiro/go-simple-RESTful-api/handler"
 )
 
 func main() {
-	config := driver.DBConnectionConfig{}
-	err := envconfig.Process("go_simple_restful", &config)
-	if err != nil {
-		log.Fatal(err.Error())
-		os.Exit(-1)
-	}
+	// config := driver.DBConnectionConfig{}
+	// err := envconfig.Process("go_simple_restful", &config)
+	// if err != nil {
+	// 	log.Fatal(err.Error())
+	// 	os.Exit(-1)
+	// }
 
-	connection, err := driver.ConnectGorm(config)
-	if err != nil {
-		fmt.Printf("Failed to connect to database: %v\n", err)
-		os.Exit(-1)
-	}
+	//connection, err := driver.ConnectGorm(config)
+	//if err != nil {
+	//	fmt.Printf("Failed to connect to database: %v\n", err)
+	//	os.Exit(-1)
+	//}
 
-	r := routers(connection)
+	// r := routers(connection)
+
+	r := chi.NewRouter()
+	r.Use(middleware.Recoverer)
+	r.Use(middleware.Logger)
+
+	r.Get("/ping", ping)
+
 	fmt.Println("Server listen at :8005")
 	if err := http.ListenAndServe(":8005", r); err != nil {
 		fmt.Println(err)
@@ -41,9 +44,9 @@ func routers(db *driver.DB) *chi.Mux {
 	r.Use(middleware.Recoverer)
 	r.Use(middleware.Logger)
 
-	nHandler := handler.NewNoteHandler(db)
-
 	r.Get("/ping", ping)
+
+	nHandler := handler.NewNoteHandler(db)
 	r.Get("/notes", nHandler.Fetch)
 	r.Get("/notes/{id}", nHandler.GetByID)
 	r.Post("/notes", nHandler.Create)
